72ad88cbb02d3eb677334ba8a689d3aeccaa2163
arijarbi12 <arij.arbi@esprit.tn>
2024-09-24 20:14:49 +0200

    third commit

---
+++ src/main/java/tn/esprit/foyeresprit/entities/Universite.java
@@ -0,0 +1,25 @@
+package tn.esprit.foyeresprit.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Universite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUniversite;
+    private String nom;
+    private String adresse;
+
+}
